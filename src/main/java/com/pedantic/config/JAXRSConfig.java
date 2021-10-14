4ae397e7f8d8f62587a7816b8cd3ed77cfc891a5
Júlio Moura <juliomourajom@gmail.com>
2021-10-14 20:21:38 -0300

    :sparkles: Hello world controller

---
+++ src/main/java/com/pedantic/config/JAXRSConfig.java
@@ -0,0 +1,8 @@
+package com.pedantic.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api/v1")
+public class JAXRSConfig extends Application {
+}
